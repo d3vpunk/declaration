ca557addcce27c97a369c127d3b8827af87e8a0d
Johannes <johannes.reiners@webstores.nl>
2024-08-03 13:43:11 +0200

    gateway script optimized

--- gateway_check.sh
+++ gateway_check.sh
@@ -8,17 +8,27 @@ fetch_gateway() {
     elif [[ "$OSTYPE" == "darwin"* ]]; then
         route -n get default | grep 'gateway' | awk '{print $2}'
     else
-        log "Unsupported operating system"
+        echo "Unsupported operating system"
         return 1
     fi
 }
 
 url_encode() {
-    printf '%s' "$1" | jq -sRr @uri
+    local length="${#1}"
+    for (( i = 0; i < length; i++ )); do
+        local c="${1:i:1}"
+        case "$c" in
+            [a-zA-Z0-9.~_-]) printf '%s' "$c" ;;
+            *) printf '%%%02X' "'$c" ;;
+        esac
+    done
 }
 
+
 base_url="https://d.d3vpunk.com/register-day"
+#base_url="https://localhost/register-day"
 query_params=""
+
 
 # Build query parameters
 for arg in "$@"; do
@@ -28,31 +38,33 @@ for arg in "$@"; do
         encoded_value=$(url_encode "$value")
         query_params="${query_params:+$query_params&}${key}=${encoded_value}"
     else
-        log "Warning: Skipping invalid parameter: $arg"
+        echo "Warning: Skipping invalid parameter: $arg"
     fi
 done
 
 final_url="${base_url}${query_params:+?$query_params}"
 
 target_gateway="172.16.16.1"
+#target_gateway="192.168.178.1"
 gateway=$(fetch_gateway)
 
 if [ $? -ne 0 ]; then
-    log "Failed to fetch gateway. Exiting."
+    echo "Failed to fetch gateway. Exiting."
     exit 1
 fi
 
 if [[ "$gateway" == "$target_gateway" ]]; then
-    log "Default gateway matches target. Calling endpoint."
+    echo "Default gateway matches target. Calling endpoint!"
+    echo "URL: $final_url"
     response=$(curl -s -w "\n%{http_code}" "$final_url")
     http_code=$(echo "$response" | tail -n1)
     body=$(echo "$response" | sed '$d')
 
     if [[ "$http_code" == "200" ]]; then
-        log "Successfully called endpoint. Response: $body"
+        echo "Successfully called endpoint. Response: $body"
     else
-        log "Failed to call endpoint. HTTP status: $http_code, Response: $body"
+        echo "Failed to call endpoint. HTTP status: $http_code, Response: $body"
     fi
 else
-    log "Default gateway ($gateway) does not match target ($target_gateway). Exiting."
+    echo "Default gateway ($gateway) does not match target ($target_gateway). Exiting."
 fi
